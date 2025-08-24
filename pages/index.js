82897b25cf4fefee455716aad5e76c918fdf12a7
Tomas <tomaslonnblad@gmail.com>
2025-08-24 19:53:39 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ export default function Home() {
           <VideoPlayer src="https://tomaslonnblad.github.io/A/index.m3u8" />
      <VideoPlayer src="https://tomaslonnblad.github.io/B/index.m3u8" />
     <VideoPlayer src="https://tomaslonnblad.github.io/C/index.m3u8" />
+    <VideoPlayer src="https://tomaslonnblad.github.io/D/index.m3u8" />
         </div>
       </main>
 
